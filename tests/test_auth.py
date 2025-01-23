aa5f40818bd5b28f46d13d8c4c6068b2115f80b6
GUERIBOUT MATHIEU <gueribout.mathieu@protonmail.com>
2025-01-23 18:15:45 +0000

    fix: improve auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,7 @@
 import pytest
+from datetime import datetime, timedelta, timezone
+from fastapi import HTTPException
+from jwt import encode  # pyright: ignore
 from jwt.exceptions import ExpiredSignatureError
 from pytest_mock import MockerFixture
 
@@ -94,8 +97,42 @@ def test_refresh_access_token_invalid_refresh_token(auth: Auth):
     assert str(e.value) == "Invalid token"
 
 
+def test_refresh_access_token_missing_data(auth: Auth):
+    expire = datetime.now(timezone.utc) + timedelta(days=30)
+    bad_token = encode({"expire": expire.isoformat()}, "secret_key", algorithm="HS256")
+    with pytest.raises(AuthException) as e:
+        auth.refresh_access_token(bad_token)
+    assert str(e.value) == "Missing username in token"
+
+
 @pytest.mark.asyncio
 async def test_get_current_user(auth: Auth):
     token = auth.create_access_token({"username": "username"})
     username = await auth.get_current_user(token)
     assert username == "username"
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_invalid_token(auth: Auth):
+    with pytest.raises(HTTPException) as e:
+        await auth.get_current_user("invalid_token")
+    assert str(e.value) == "401: Invalid authentication credentials"
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_expired_token(auth: Auth, mocker: MockerFixture):
+    # I failed to reproduce token expiration error so let's mock it
+    mocker.patch("python_utils.auth.decode", side_effect=ExpiredSignatureError)
+    token = auth.create_access_token({"username": "username"})
+    with pytest.raises(HTTPException) as e:
+        await auth.get_current_user(token)
+    assert str(e.value) == "401: Invalid authentication credentials"
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_invalid_credentials(auth: Auth):
+    expire = datetime.now(timezone.utc) + timedelta(days=30)
+    bad_token = encode({"expire": expire.isoformat()}, "secret_key", algorithm="HS256")
+    with pytest.raises(HTTPException) as e:
+        await auth.get_current_user(bad_token)
+    assert str(e.value) == "401: Invalid authentication credentials"
